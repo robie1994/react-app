9d0322bdd9f55b7e7f782dc6e0675ab1c618e6f6
Robie Mendiola <rmendiola@joinpiggy.com>
2021-12-20 22:59:06 +0800

    commit 3

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Modal from 'react-modal'
 import $ from 'jquery';
 import { CloseButton } from 'react-bootstrap';
